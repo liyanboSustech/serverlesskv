d596822a0c6d2102b0f22babf8c3140328884c36
liyanbo <197422352@qq.com>
2024-08-27 11:13:02 +0000

    update

---
+++ Asiatkv/h2o_hf/utils_hh/run_model_generation.py
@@ -0,0 +1,35 @@
+import copy
+from transformers import AutoModelForCausalLM, AutoTokenizer,AutoConfig
+from modify_llama import convert_kvcache_llama_heavy_recent
+
+model_path = "/dataset/crosspipe/OriginModel/Llama-2-7b-chat-hf/"
+
+prompt_text = 'In a small, bustling cafe nestled in the heart of a vibrant city, a serendipitous event unfolded, leaving a lasting impression on all who witnessed it. As the patrons sat sipping their coffees and engaging in animated conversations, a talented street musician entered the cafe, carrying a weathered guitar and radiating an aura of creativity.'
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
+
+######## Generate with Full Cache
+model = AutoModelForCausalLM.from_pretrained(model_path)
+model.half().eval().cuda()
+
+# input_ids = tokenizer(prompt_text, return_tensors='pt').input_ids.to(model.device)
+input_ids = tokenizer(prompt_text, add_special_tokens=False, return_tensors='pt').input_ids.to(model.device)
+
+generate_ids = model.generate(input_ids, max_new_tokens=64)
+result = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+print("################## Generated Context with Full Cache ###################")
+
+
+print(result)
+print("loading checkpoint")
+checkpoint = copy.deepcopy(model.state_dict())
+config = AutoConfig.from_pretrained(model_path)
+config.heavy_ratio = 0.1
+config.recent_ratio = 0.1
+model = convert_kvcache_llama_heavy_recent(model, config)
+checkpoint = copy.deepcopy(model.state_dict())
+model.load_state_dict(checkpoint)
+generate_ids_hh = model.generate(input_ids, max_new_tokens=64)
+result_hh = tokenizer.batch_decode(generate_ids_hh, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+print("################## Generated Context with Heavy Hitter Oracle ###################")
+print(result_hh)
---
+++ Past_key_value_mechanism/_mask_causal_mask.py
@@ -0,0 +1,14 @@
+import torch
+def _make_causal_mask(
+    bsz: int, tgt_len: int, past_key_values_length: int, dtype: torch.dtype, device: torch.device):
+    """
+    Make causal mask used for bi-directional self-attention.
+    """
+    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+    mask_cond = torch.arange(mask.size(-1), device=device)
+    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+    mask = mask.to(dtype)
+
+    if past_key_values_length > 0:
+        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
